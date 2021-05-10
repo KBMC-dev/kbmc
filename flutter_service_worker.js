02a3a738f0628a1b70e4fa3fb8a14e9f188a1551
Brandyux <Brandyux@users.noreply.github.com>
2021-05-10 20:20:51 +0000

    deploy: e56d4c63a1524a90b5e6d7ebfe13d1c9760b8696

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,19 +3,19 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "a8d1d130b9909728d724354dc06becc1",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/NOTICES": "6bb7fa08eaad6bfa999420a186588c07",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "812fd5fe5c168ef1ad57fe50e91ead15",
-"/": "812fd5fe5c168ef1ad57fe50e91ead15",
-"main.dart.js": "606d33c4035a180d3665086f72afa328",
-"manifest.json": "feaa6f2d6b422d55cf67ecad0321c30a",
-"version.json": "6c4d27c8657608c04716364e09adf9aa"
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"version.json": "6c4d27c8657608c04716364e09adf9aa",
+"manifest.json": "3b641fea055776d6a5eb055dd2452efb",
+"main.dart.js": "2a51deaf532934292dbcb7f820596c15",
+"index.html": "94acafefd585b1671db4ade9e289f4d8",
+"/": "94acafefd585b1671db4ade9e289f4d8"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -33,7 +33,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
