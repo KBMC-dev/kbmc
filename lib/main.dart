82335e7d76d010afa5e9a05270f6d0c222e426a3
Brandyux <696brandy@gmail.com>
2021-05-11 13:08:41 -0400

    11

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'KBMC',
       theme: ThemeData.dark(),
       home: MyHomePage(title: 'KBMC'),
     );
