551be69f96925386e0321f1861b5ce8d092ce08d
Brandyux <696brandy@gmail.com>
2021-05-11 12:47:03 -0400

    10

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Icon(Icons.access_alarms),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
